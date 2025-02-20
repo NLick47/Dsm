2a62debec01e8148bb9cafa4f7ca864ead7afb5e
Nlick47 <2247717951@qq.com>
2025-02-20 15:44:28 +0800

    初始

---
+++ Dsm/Music.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Dsm;
+
+public partial class Music : UserControl
+{
+    public Music()
+    {
+        InitializeComponent();
+    }
+}
